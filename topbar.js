6a86716b69b2b458337a7e41c026fd56f58f11b5
a-anmol <learnwithanmol@gmail.com>
2022-07-28 20:25:53 +0530

    nav links and theme button update

--- topbar.js
+++ topbar.js
@@ -3,3 +3,23 @@ function removeNotice() {
 }
 
 
+function changeIcon(from, to) {
+      const icon = document.querySelector('#icon')
+      icon.addEventListener('click', (e) => {
+            const themeText = document.querySelector('.themeText')
+            themeText.textContent === 'Dark' ?
+                  themeText.textContent = 'Light' :
+                  themeText.textContent = 'Dark'
+
+
+            let iconClass = e.currentTarget.children[1].children[1].children[0];
+            iconClass.getAttribute('href') === from ?
+                  iconClass.setAttribute('href', to) :
+                  iconClass.setAttribute('href', from);
+
+      })
+}
+changeIcon('#moon', '#sun')
+
+
+
