f3f5992ae8fc9eb6836fdd6f1190da1510e44bf4
a-anmol <learnwithanmol@gmail.com>
2022-11-22 16:17:15 +0530

    flesh screen and  theme dark

--- topbar.js
+++ topbar.js
@@ -3,23 +3,58 @@ function removeNotice() {
 }
 
 
-function changeIcon(from, to) {
-      const icon = document.querySelector('#icon')
-      icon.addEventListener('click', (e) => {
-            const themeText = document.querySelector('.themeText')
-            themeText.textContent === 'Dark' ?
-                  themeText.textContent = 'Light' :
-                  themeText.textContent = 'Dark'
+// function changeIcon(from, to) {
+//       const icon = document.querySelector('#icon')
+//       icon.addEventListener('click', (e) => {
+//             const themeText = document.querySelector('.themeText')
+//             themeText.textContent === 'Dark' ?
+//                   themeText.textContent = 'Light' :
+//                   themeText.textContent = 'Dark'
 
 
-            let iconClass = e.currentTarget.children[1].children[1].children[0];
-            iconClass.getAttribute('href') === from ?
-                  iconClass.setAttribute('href', to) :
-                  iconClass.setAttribute('href', from);
+//             let iconClass = e.currentTarget.children[1].children[1].children[0];
+//             iconClass.getAttribute('href') === from ?
+//                   iconClass.setAttribute('href', to) :
+//                   iconClass.setAttribute('href', from);
 
-      })
+//       })
+// }
+// changeIcon('#moon', '#sun')
+
+
+
+const themeText = document.querySelector('.themeText')
+//             themeText.textContent === 'Dark' ?
+//                   themeText.textContent = 'Light' :
+//                   themeText.textContent = 'Dark'
+const darkTheme ="darkTheme";
+const icon = document.querySelector("#icon");
+
+const selectedTheme=localStorage.getItem("selectedTheme");
+
+
+const getCurrentTheme=()=>document.body.classList.contains(darkTheme)?"dark":"light";
+const getCurrentIcon=()=>document.body.classList.contains(darkTheme)?"#moon":"#sun";
+
+if(selectedTheme){
+      document.body.classList[selectedTheme==="dark"?'add':'remove'](darkTheme)
+      icon.children[1].children[1].children[0].setAttribute("href",[selectedTheme==="dark"?'#moon':'#sun']);
+      
 }
-changeIcon('#moon', '#sun')
 
 
+icon.addEventListener("click",()=>{
+      document.body.classList.toggle(darkTheme);
+      icon.children[1].children[1].children[0].getAttribute("href")==="#moon"
+      localStorage.setItem("selectedTheme",getCurrentTheme())
+})
 
+
+const closePrompt=document.querySelector(".closePrompt");
+const blurArea=document.querySelector(".blurArea");
+const promptMessage=document.querySelector(".promptMessage");
+
+closePrompt.addEventListener("click",()=>{
+      blurArea.remove();
+      promptMessage.remove();
+})
